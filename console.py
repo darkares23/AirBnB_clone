d5882cd03957eee70034d4538cd436dd971c9d78
Mary Serna <mary.serna.8903@gmail.com>
2020-02-15 14:30:33 -0500

    add all method

--- console.py
+++ console.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 import cmd
 import shlex
+
 import models
-from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.city import City
 from models.amenity import Amenity
+from models.base_model import BaseModel
+from models.city import City
 from models.place import Place
 from models.review import Review
+from models.state import State
+from models.user import User
 
 
 class HBNBCommand(cmd.Cmd):
@@ -93,7 +94,8 @@ class HBNBCommand(cmd.Cmd):
                 if args[0] in key:
                    obj_list.append(str(models.storage.all()[key]))
             print(obj_list)
-
+        else:
+            print("** class doesn't exist **")
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
