2c2627cd1a0536ba1c00d4cc4c0196b3bc7dd2d7
Mary Serna <mary.serna.8903@gmail.com>
2020-02-15 11:24:52 -0500

    entry point of the command interpreter

--- console.py
+++ console.py
@@ -1,5 +1,24 @@
 #!/usr/bin/python3
-"""
-Interpreter console
-"""
+import cmd
+import shlex
 
+class HBNBCommand(cmd.Cmd):
+
+    prompt = '(hbnb) '
+
+    classes = ['BaseModel', 'User', 'State', 'City', 'Amenity', 'Place', 'Review']
+
+    def do_EOF(self, line):
+        """Command to exit the Interpreter typing EOF"""
+        return True
+
+    def do_quit(self, line):
+        """Quit command to exit the program\n"""
+        return True
+
+    def emptyline(self):
+        """empty line + ENTER execute anything"""
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
