8beb7894fca9b7d48d12d663cdce0320a7cf7bd3
Juan Ocampo <desarrollojuseca@gmail.com>
2020-02-18 16:17:20 -0500

    fixed pep8 test files

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -10,13 +10,10 @@ from models.state import State
 from models.review import Review
 
 
-
 class Testcity(unittest.TestCase):
 
-	def test_pep8_conformance_city(self):
-    	"""Test that we conform to PEP8."""
-    	pep8style = pep8.StyleGuide(quiet=True)
-    	result = pep8style.check_files(['models/city.py'])
-    	self.assertEqual(result.total_errors, 0,
-                      "Found code style errors (and warnings).")
-
+    def test_pep8_conformance_city(self):
+        """Test that we conform to PEP8."""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['models/city.py'])
+        self.assertEqual(result.total_errors, 0, "Found style errors")
