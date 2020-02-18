8a5725670be184191be64f273aa95dfacf6db103
maryserar8903 <mary.serna.8903@gmail.com>
2020-02-18 20:18:32 +0000

    test

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -13,7 +13,44 @@ from models.review import Review
 class Testpep8(unittest.TestCase):
 
     def test_pep8_conformance_base_model(self):
-            """Test that we conform to PEP8."""
-            pep8style = pep8.StyleGuide(quiet=True)
-            result = pep8style.check_files(['models/base_model.py'])
-            self.assertEqual(result.total_errors, 0, "Found style errors.")
+        """Test that we conform to PEP8."""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['models/base_model.py'])
+        self.assertEqual(result.total_errors, 0, "Found style errors.")
+
+    def test_pep8_base_model(self):
+        """ Test for PEP8 ok. """
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['models/base_model.py'])
+        self.assertEqual(result.total_errors, 0, "Please fix pep8")
+
+    def test_isinstance(self):
+        """"Test if is an instance of the class"""
+        obj = BaseModel()
+        self.assertIsInstance(obj, BaseModel)
+
+    def test_init(self):
+        """Test __init__
+        """
+        base = BaseModel()
+        self.assertTrue(hasattr(base, "id"))
+        self.assertTrue(hasattr(base, "created_at"))
+        self.assertTrue(hasattr(base, "updated_at"))
+
+    def test_id(self):
+        """Test id
+        """
+        base0 = BaseModel()
+        base1 = BaseModel()
+        self.assertEqual(uuid.UUID(base0.id).version, 4)
+        self.assertFalse(base0.id == base1.id)
+
+    def test_to_dict(self):
+        """Tests the to_dict function."""
+        obj = BaseModel()
+        new_dict = obj.__dict__.copy()
+        new_dict["__class__"] = obj.__class__.__name__
+        new_dict["created_at"] = new_dict["created_at"].isoformat()
+        new_dict["updated_at"] = new_dict["updated_at"].isoformat()
+        comparing = obj.to_dict()
+        self.assertDictEqual(new_dict, comparing)
